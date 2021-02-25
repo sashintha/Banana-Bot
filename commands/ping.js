542631b1b4fb2df432f3854e36f5ebc67a3848f0
Sashintha Tennakoon <sashitnha2001@gmail.com>
2021-02-25 13:20:40 -0500

    final fix

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports ={
     name: 'gretting',
     description: "this is a ping command!",
     execute(message, args){
-        message.channel.send("test test")
+        message.channel.send("test 2")
     }
 }
