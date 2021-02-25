2690c5dc61df3419ee94b8faf8a7b47a77ab189f
Sashintha Tennakoon <sashitnha2001@gmail.com>
2021-02-25 02:36:18 -0500

    fixing token file

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports ={
     name: 'gretting',
     description: "this is a ping command!",
     execute(message, args){
-        message.channel.send("hello there :)")
+        message.channel.send("test test")
     }
 }
